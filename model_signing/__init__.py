b7ad17372a6836423ef31d1996e2d94b65e42aa5
Mihai Maruseac <mihaimaruseac@google.com>
2024-08-07 10:02:00 -0700

    Attempt to build the wheel.
    There are many TODOs, so this is just in draft mode.
    
    TODO: Edit this message before making a PR.
    
    To build the wheel, `pip install build` and then `python -m build`
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

--- model_signing/__init__.py
+++ model_signing/__init__.py
@@ -11,3 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# TODO: Define public, high-level API after some testing of the library
+
+__version__ = "0.0.1-alpha"
